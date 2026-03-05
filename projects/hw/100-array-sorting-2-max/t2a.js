1ea213578f6e8e2f151f122b78b8a45e80739dfb
araf-47 <araf47.edu@gmail.com>
2026-03-05 06:42:57 +0600

    added a subpage, folder, and a hw project

---
+++ projects/hw/100-array-sorting-2-max/t2a.js
@@ -0,0 +1,9 @@
+function maxArr(){
+    // let input = parseInt(document.getElementById('textInput').value);
+   
+    // console.log(vngaVangi);
+    let input = document.getElementById('textInput').value; 
+    let vngaVangi = input.split("-");
+    console.log(vngaVangi);
+
+}
